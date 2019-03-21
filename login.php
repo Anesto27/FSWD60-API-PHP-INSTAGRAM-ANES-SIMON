4548bcf3bc7dfef43c182a4aba62e87354fe5808
Anesto27 <anes.2503@gmx.at>
2019-03-21 14:13:27 +0100

    finish login register and contact page

--- login.php
+++ login.php
@@ -2,22 +2,220 @@
 <html lang="en" dir="ltr">
 
 <head>
-    <meta charset="utf-8">
-    <title>َAnimated Login Form</title>
-    <link rel="stylesheet" href="style.css">
+	<meta charset="utf-8">
+	<title>َAnimated Login Form</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css" integrity="sha384-Mmxa0mLqhmOeaE8vgOSbKacftZcsNYDjQzuCOm6D02luYSzBG8vpaOykv9lFQ51Y" crossorigin="anonymous">
 </head>
 
 <body>
-    <video autoplay muted loop id="myVideo">
-        <source src="img/video.mp4" type="video/mp4">
-    </video>
-    <form class="box" action="index.html" method="post">
-      <img src="img/logo.png" alt="logo" width=200px height=90px>
-        <h1>Login</h1>
-        <input type="text" name="" placeholder="Username">
-        <input type="password" name="" placeholder="Password">
-        <input type="submit" name="" value="Login">
-    </form>
+	
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">OREVIDA</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Login <span class="sr-only">(current)</span></a></li>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="index.html">Contact</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<div class="videodiv">
+	<video autoplay muted loop id="myVideo" width="100%" height="900">
+		<source src="img/video.mp4" type="video/mp4">
+	</video>
+</div>
+		<form class="box1" action="index.html" method="post">
+			<img src="img/logo.png" alt="logo" width=200px height=90px>
+			<h1>Login</h1>
+			<input type="text" name="" placeholder="Username">
+			<input type="password" name="" placeholder="Password">
+			<div class="row">
+				<div class="col-md-3">
+					<input type="submit" name="" value="Login">
+				</div>
+				<div class="col-md-2">
+					<fb:login-button 
+					id="fb-btn"
+					scope="public_profile,email"
+					onlogin="checkLoginState();">
+				</fb:login-button>
+				<a id="logout" onclick="logout()" href="#">Logout</a>
+			</div>
+		</div>
+	</form>
+	<script>
+		window.fbAsyncInit = function() {
+			FB.init({
+				appId      : '2119613451455388',
+				cookie     : true,
+				xfbml      : true,
+				version    : 'v3.2'
+			});
+
+			FB.getLoginStatus(function(response) {
+				statusChangeCallback(response);
+			});
+
+		};
+
+		(function(d, s, id){
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) {return;}
+			js = d.createElement(s); js.id = id;
+			js.src = "https://connect.facebook.net/en_US/sdk.js";
+			fjs.parentNode.insertBefore(js, fjs);
+
+		}(document, 'script', 'facebook-jssdk'));
+
+		function statusChangeCallback(response) {
+			if (response.status === "connected") {
+				console.log("Logged in and authenticated");
+				<?php 
+					
+
+				 ?>
+
+				setElements(true);
+				graphAPI();
+			} else {
+				console.log("Not authenticated");
+				setElements(false);
+			}
+		}
+		function checkLoginState() {
+			FB.getLoginStatus(function(response) {
+				statusChangeCallback(response);
+			});
+		}
+
+		function setElements(isLoggedIn){
+			if(isLoggedIn){
+				document.getElementById('fb-btn').style.display = 'none';
+				document.getElementById('logout').style.display = 'block';
+			} else {
+				document.getElementById('fb-btn').style.display = 'block';
+				document.getElementById('logout').style.display = 'none';
+			}
+		}
+
+		function logout(){
+			FB.logout(function(response){
+				setElements(false);
+			})
+		}
+
+		function graphAPI(){
+			FB.api("me?fields=id,name,email" , function(response){
+				if(response && !response.error){
+					console.log(response.email);
+				}
+			})
+		}
+
+
+	</script>
+
+	<footer id="myFoot" class="footer">        
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="widget widget-text">
+                        <img src="img/logo.png" alt="logo-footer" width=200px height=190px>
+
+                    </div>
+                </div><!-- /.col-md-12 -->    
+            </div><!-- /.row -->    
+
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="sidebar-inner-footer">
+                        <div class="widget widget-infomation">
+                            <ul class="flat-information">
+                                <li class="email"><i class="fa fa-envelope" aria-hidden="true"></i> CodeFactory@gmail.com</li>
+                                <li class="phone"><i class="fa fa-phone" aria-hidden="true"></i> +436603781700</li>  
+                                <li class="address"><i class="fa fa-map-marker" aria-hidden="true"></i> Kettenbrückengasse 23/2/12 1050 Vienna</li>    
+                            </ul>
+                            <hr>
+                        </div>
+                    </div><!-- /.sidebar-inner-footer -->    
+                </div><!-- /.col-md-12 -->    
+            </div><!-- /.row -->      
+        </div><!-- /.container -->
+    
+
+    <!-- Bottom -->
+    <div class="bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="line-top"></div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="container-buttom">
+                    <div class="col-md-6">
+                        <div class="copyright"> 
+                            <p>© Copyright CodeFactory 2019. <br>All Rights Reserved.
+                            </p>
+                        </div>                   
+                    </div><!-- /.col-md-6 -->
+
+                    <div class="col-md-6">
+                        <ul class="icons">
+                                <li>
+                                    <a href="https://www.facebook.com/CodeFactoryVienna/">
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                        <span class="fab fa-facebook-f" aria-hidden="true"></span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.instagram.com/codefactoryvienna/">
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                        <span class="fab fa-instagram" aria-hidden="true"></span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://twitter.com/codefactory?lang=de">
+                                        
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                        <span class="fab fa-twitter" aria-hidden="true"></span>
+                                    </a>
+                                </li>
+                            </ul>   
+                    </div><!-- /.col-md-6 -->
+                </div><!-- /.container-bottom -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div>
+     </footer>
 </body>
 
 </html>
--- register.php
+++ register.php
@@ -4,14 +4,46 @@
 <head>
     <meta charset="utf-8">
     <title>َAnimated Login Form</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css" integrity="sha384-Mmxa0mLqhmOeaE8vgOSbKacftZcsNYDjQzuCOm6D02luYSzBG8vpaOykv9lFQ51Y" crossorigin="anonymous">
 </head>
 
 <body>
-    <video autoplay muted loop id="myVideo">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">OREVIDA</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="login.php">Login <span class="sr-only">(current)</span></a></li>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="index.html">Contact</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+    <video autoplay muted loop id="myVideo" width="100%" height="900">
         <source src="img/video.mp4" type="video/mp4">
     </video>
-    <form class="box" action="index.html" method="post">
+
+    <form class="box1" action="index.html" method="post">
       <img src="img/logo.png" alt="logo" width=200px height=90px>
         <h1>Register</h1>
         <input type="text" name="" placeholder="Username">
@@ -21,5 +53,85 @@
         <input type="submit" name="" value="Login">
     </form>
 </body>
+
+<footer id="myFoot" class="footer">        
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="widget widget-text">
+                        <img src="img/logo.png" alt="logo-footer" width=200px height=190px>
+
+                    </div>
+                </div><!-- /.col-md-12 -->    
+            </div><!-- /.row -->    
+
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="sidebar-inner-footer">
+                        <div class="widget widget-infomation">
+                            <ul class="flat-information">
+                                <li class="email"><i class="fa fa-envelope" aria-hidden="true"></i> CodeFactory@gmail.com</li>
+                                <li class="phone"><i class="fa fa-phone" aria-hidden="true"></i> +436603781700</li>  
+                                <li class="address"><i class="fa fa-map-marker" aria-hidden="true"></i> Kettenbrückengasse 23/2/12 1050 Vienna</li>    
+                            </ul>
+                            <hr>
+                        </div>
+                    </div><!-- /.sidebar-inner-footer -->    
+                </div><!-- /.col-md-12 -->    
+            </div><!-- /.row -->      
+        </div><!-- /.container -->
+    
+
+    <!-- Bottom -->
+    <div class="bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="line-top"></div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="container-buttom">
+                    <div class="col-md-6">
+                        <div class="copyright"> 
+                            <p>© Copyright CodeFactory 2019. <br>All Rights Reserved.
+                            </p>
+                        </div>                   
+                    </div><!-- /.col-md-6 -->
+
+                    <div class="col-md-6">
+                        <ul class="icons">
+                                <li>
+                                    <a href="https://www.facebook.com/CodeFactoryVienna/">
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                        <span class="fab fa-facebook-f" aria-hidden="true"></span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.instagram.com/codefactoryvienna/">
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                        <span class="fab fa-instagram" aria-hidden="true"></span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://twitter.com/codefactory?lang=de">
+                                        
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                        <span class="fab fa-twitter" aria-hidden="true"></span>
+                                    </a>
+                                </li>
+                            </ul>   
+                    </div><!-- /.col-md-6 -->
+                </div><!-- /.container-bottom -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div>
+     </footer>
 
 </html>
