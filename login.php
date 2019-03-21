e426a61d6247e9932ed5c6477a7b4905fa274ebc
Anesto27 <anes.2503@gmx.at>
2019-03-21 09:28:14 +0100

    add design for login and register page

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>َAnimated Login Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <video autoplay muted loop id="myVideo">
+        <source src="img/video.mp4" type="video/mp4">
+    </video>
+    <form class="box" action="index.html" method="post">
+      <img src="img/logo.png" alt="logo" width=200px height=90px>
+        <h1>Login</h1>
+        <input type="text" name="" placeholder="Username">
+        <input type="password" name="" placeholder="Password">
+        <input type="submit" name="" value="Login">
+    </form>
+</body>
+
+</html>
